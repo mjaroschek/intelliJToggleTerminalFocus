f79f7c7a522a039f71f70110dc6c8a11079d40cf
GitHub Action <action@github.com>
2020-12-04 08:49:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/mjaroschek/intellijtoggleterminalfocus/MyBundle.kt
+++ src/main/kotlin/com/github/mjaroschek/intellijtoggleterminalfocus/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mjaroschek.intellijtoggleterminalfocus
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/mjaroschek/intellijtoggleterminalfocus/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mjaroschek.intellijtoggleterminalfocus.services
+
+import com.github.mjaroschek.intellijtoggleterminalfocus.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
